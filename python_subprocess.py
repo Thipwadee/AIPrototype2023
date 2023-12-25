3a2c31020af471d148abd2669a4ea3ec18a55348
Thipwadee <Thipwadee.k@kkumail.com>
2023-12-25 16:19:43 +0700

    test subprocess

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,3 +3,26 @@ import subprocess #สำหรับrun therminal command
 if __name__ == "__main__":
    #basic therminal command
    subprocess.run(["ls", "-ltr"])
+
+   print("first run num=100 xx=90")
+   subprocess.run(["python", "fristpy.py", "--num", "100","-xx","90"])
+   print("----------------------------------------------------------") 
+   print("secord run num=10 xx=90")
+   subprocess.run(["python", "fristpy.py", "--num", "10","-xx","-90"])
+   print("----------------------------------------------------------") 
+   print("third run num=0 ")
+   subprocess.run(["python", "fristpy.py", "--num", "0"])
+
+#use output from other program
+   process_output = subprocess.Popen(["python", "fristpy.py", "--num", "0"],
+                                      stdout = subprocess.PIPE,
+                                      stderr = subprocess.PIPE)
+   
+   out, err = process_output.communicate()
+   print(out.decode('utf-8'))
+   print(len(out.decode('utf-8')))
+
+
+
+
+     
