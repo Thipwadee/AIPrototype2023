a50e8f710bef60a447a1565ad365748b54270f7c
Thipwadee <Thipwadee.k@kkumail.com>
2024-01-16 10:48:11 +0700

    firstflask

--- firstflask.py
+++ firstflask.py
@@ -17,13 +17,20 @@ def hellonuey():
 
 @app.route("/home", methods=['POST','GET'])
 def homefn():
-    print('we aer in home', file=sys.stdout)
+    if request.methods == "GET":
+       print('we aer in home(GET)', file=sys.stdout)
 
-    namein = request.form.get('fname')
-    lastnamein = request.form.get('lname')
-    print(namein, file=sys.stdout)
-    print(lastnamein, file=sys.stdout)
-    return render_template("home.html", name=namein)
+       namein = request.args.get('fname')
+       print(namein, file=sys.stdout)
+       return render_template("home.html", name=namein)
+
+    elif request.methods == "POST":
+       print('we aer in home(POST)', file=sys.stdout)
+       namein = request.form.get('fname')
+       lastnamein = request.form.get('lname')
+       print(namein, file=sys.stdout)
+       print(lastnamein, file=sys.stdout)
+       return render_template("home.html", name=namein)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True,port=5001) #host='0.0.0.0'คือสามารถให้เครื่องอื่นเห็นได้
