c4dc954c6bf2efa2f7ad51b87511ee0c2a872675
Thipwadee <Thipwadee.k@kkumail.com>
2024-01-16 11:12:00 +0700

    firstflask

--- firstflask.py
+++ firstflask.py
@@ -1,6 +1,6 @@
 from crypt import methods
 from doctest import debug
-from flask import Flask, request, render_template, make_response
+from flask import Flask, flash,request, redirect, render_template, make_response
 
 import json
 import sys
@@ -31,6 +31,33 @@ def homefn():
        print(namein, file=sys.stdout)
        print(lastnamein, file=sys.stdout)
        return render_template("home.html", name=namein)
+    
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        #if 'file' not in request.files:
+        #    flash('No file part')
+        #    return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        #if file.filename == '':
+        #    flash('No selected file')
+        #    return redirect(request.url)
+        file.save('filename')
+        return render_template("home.html", name='upload completed')
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''   
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True,port=5001) #host='0.0.0.0'คือสามารถให้เครื่องอื่นเห็นได้
