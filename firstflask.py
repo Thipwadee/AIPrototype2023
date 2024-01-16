33d7f6edb69afe3e49bb7f4ef37e43ba96eb3c1a
Thipwadee <Thipwadee.k@kkumail.com>
2024-01-16 09:52:25 +0700

    firstflask

--- firstflask.py
+++ firstflask.py
@@ -18,10 +18,10 @@ def hellonuey():
 def homefn():
     print('we aer in home')
     namein = request.form.get('fname')
-    lastnamein = request.form.get('fname')
+    lastnamein = request.form.get('lname')
     print(namein)
     print(lastnamein)
-    return render_template("home.html", name='nuey')
+    return render_template("home.html", name=namein)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True,port=5001) #host='0.0.0.0'คือสามารถให้เครื่องอื่นเห็นได้
