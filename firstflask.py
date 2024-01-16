3457b46d6909e3eeec7c8134496340fe1df6e2c6
Thipwadee <Thipwadee.k@kkumail.com>
2024-01-16 11:28:33 +0700

    firstflask

--- firstflask.py
+++ firstflask.py
@@ -7,6 +7,17 @@ import sys
 
 app = Flask(__name__)
 
+##api
+@app.route('/request',methods=['POST'])
+def web_service_API():
+
+    payload = request.data.decode("utf-8")
+    imessage = json.loads(payload)
+
+    print(imessage)
+    json_data = json.dumps({'y':'received'})
+    return json_data
+
 @app.route("/")
 def helloworld():
     return "Hello World!"
