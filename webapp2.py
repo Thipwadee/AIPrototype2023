b9e0618a1a002e47bde8547d5c6bb1247d222687
Thipwadee <Thipwadee.k@kkumail.com>
2024-02-09 21:48:58 +0700

    web app

--- webapp2.py
+++ webapp2.py
@@ -22,6 +22,23 @@ def web_service_API():
 def helloworld():
     return "<h1>Hello! Welcome to My Web Application.</h1>"
 
+@app.route("/home", methods=['POST','GET'])
+def homefn():
+    if request.method == "GET":
+       print('we aer in home(GET)', file=sys.stdout)
+
+       namein = request.args.get('fname')
+       print(namein, file=sys.stdout)
+       return render_template("home.html", name=namein)
+
+    elif request.method == "POST":
+       print('we aer in home(POST)', file=sys.stdout)
+       namein = request.form.get('fname')
+       lastnamein = request.form.get('lname')
+       print(namein, file=sys.stdout)
+       print(lastnamein, file=sys.stdout)
+       return render_template("home.html", name=namein)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True,port=5001) #host='0.0.0.0'คือสามารถให้เครื่องอื่นเห็นได้
