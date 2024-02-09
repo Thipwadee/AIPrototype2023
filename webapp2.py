8cfad21e6f5b88fc87c2167c228183a9215e7735
Thipwadee <Thipwadee.k@kkumail.com>
2024-02-09 18:44:19 +0700

    web app

--- webapp2.py
+++ webapp2.py
@@ -22,21 +22,6 @@ def web_service_API():
 def helloworld():
     return "<h1>Hello! Welcome to My Web Application.</h1>"
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        #if 'file' not in request.files:
-        #    flash('No file part')
-        #    return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
-        #if file.filename == '':
-        #    flash('No selected file')
-        #    return redirect(request.url)
-        file.save('filename')
-        return "<h1>อัพโหลดไฟล์สำเร็จ</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True,port=5001) #host='0.0.0.0'คือสามารถให้เครื่องอื่นเห็นได้
