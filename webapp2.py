820470a6c823af1ca342bc2c01a4ccf1a36d99aa
Thipwadee <Thipwadee.k@kkumail.com>
2024-03-02 23:05:06 +0700

    web app

--- webapp2.py
+++ webapp2.py
@@ -1,11 +1,12 @@
 from crypt import methods
 from doctest import debug
 from flask import Flask, flash,request, redirect, render_template, make_response
-
+import pickle
 import json
 import sys
 
 app = Flask(__name__)
+model = pickle.load(open('model (1).pk','rb'))
 
 ##api
 @app.route('/request',methods=['POST'])
@@ -28,24 +29,55 @@ def homefn():
 
 @app.route("/form", methods=['POST','GET'])
 def form_info():
-    #age = request.form['agein']
-    #weight = request.form['weightin']
-    #height = request.form['heightin']
-
-    if request.method == "GET":
-       print('here(GET)', file=sys.stdout)
+    if request.method == "POST":
+        #get form data
+        gender = request.form.get('gender')
+        age = request.form.get('agein')
+        weight = request.form.get('weightin')
+        height = request.form.get('heightin')
+        bmi = request.form.get('bmiin')
+        temp= request.form.get('tempin')
+        rh = request.form.get('rhin')
+        v = request.form.get('vin')
+        tmrt = request.form.get('tmrtin')
+        area= request.form.get('area')
+        result = model.predict([[gender, age, weight, height, bmi, temp,rh,v,tmrt,area]])[0]
+        return render_template('pred.html')
+        
+        #prediction = preprocessDataAndPredict(gender, age, weight, height, bmi, temp,rh,v,tmrt,area)
+            #pass prediction to template
+        #return render_template('result.html', prediction = prediction)
+    #if request.method == "GET":
+    #   print('here(GET)', file=sys.stdout)
     
-       Agein = request.args.get('ticketNum')
-       print(Agein, file=sys.stdout)
-       return render_template("pred.html")
+    #   Agein = request.args.get('ticketNum')
+    #   print(Agein, file=sys.stdout)
+    #   return render_template("pred.html")
       
-    elif request.method == "POST":
-       print('here (POST)', file=sys.stdout)
-       Age = request.form.get('agein')
-       weight = request.form.get('weightin')
-       print(Age)
-       print(weight)
-       return render_template("result.html")
+    #elif request.method == "POST":
+    #   print('here (POST)', file=sys.stdout)
+    #   Age = request.form.get('agein')
+    #   weight = request.form.get('weightin')
+    #   print(Age)
+    #   print(weight)
+    #   return render_template("result.html")
+
+#def preprocessDataAndPredict(gender, age, weight, height, bmi, temp,rh,v,tmrt,area):
+#    #put all inputs in array
+#    test_data = [[gender, age, weight, height, bmi, temp,rh,v,tmrt,area]]
+#    print(test_data)
+#    #convert value data into numpy array
+#    test_data = np.array(test_data)
+#    #creating a dataframe
+#    test_data = pd.DataFrame(test_data)
+#    print(test_data)
+#    #open file
+#    file = open("model.pk","rb")
+#    #load trained model
+#    trained_model = joblib.load(file)
+#    #predict
+#    prediction = trained_model.predict(test_data)
+#    return render_template("result.html")
 
 
 @app.route("/res", methods=['POST','GET'])
