21cbe20e2db2660c19eb37c63e990864aa53bd44
Thipwadee <Thipwadee.k@kkumail.com>
2024-03-03 00:20:31 +0700

    web app

--- webapp2.py
+++ webapp2.py
@@ -50,7 +50,7 @@ def form_info():
         #return render_template('result.html', prediction = prediction)
     #if request.method == "GET":
     #   print('here(GET)', file=sys.stdout)
-    model = pickle.load(open('model\ (1).pk','rb'))
+    model = pickle.load(open('model (1).pk','rb'))
     #   Agein = request.args.get('ticketNum')
     #   print(Agein, file=sys.stdout)
     #   return render_template("pred.html")
