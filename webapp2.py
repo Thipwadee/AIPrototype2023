c51b8822da2e3f663e2fca699d50bae1701e5a6c
Thipwadee <Thipwadee.k@kkumail.com>
2024-03-03 21:17:19 +0700

    web app

--- webapp2.py
+++ webapp2.py
@@ -8,8 +8,8 @@ from joblib import dump, load
 import sklearn
 
 app = Flask(__name__)
-with (open('../AIPrototype2023/model (1).pk', 'rb') ) as f :
-      model = load(f)
+with (open('../AIPrototype2023/model_ta.pk', 'rb') ) as f :
+      model_ta = load(f)
 ##api
 @app.route('/request',methods=['POST'])
 def web_service_API():
@@ -59,7 +59,7 @@ def form_info():
         print(tmrt,file=sys.stdout)
         print(area,file=sys.stdout)
         print(area,file=sys.stdout)
-        result = model.predict([[gender, age, weight, height, bmi, temp,rh,v,tmrt,area,seasons]])[0]
+        result = model_ta.predict([[gender, age, weight, height, bmi, temp,rh,v,tmrt,area,seasons]])[0]
         return render_template('result.html') #,gender=gender, age=age, weight=weight, height=height, bmi=bmi, temp=temp,rh=rh,v=v,tmrt=tmrt,area=area)
         
     #prediction = preprocessDataAndPredict(gender, age, weight, height, bmi, temp,rh,v,tmrt,area)
